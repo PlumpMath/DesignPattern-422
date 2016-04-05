a863d019980fa25f35f05b622551dea251c4c9c3
zxiaofan <zeng-dream@live.com>
2016-04-05 10:26:50 +0800

    单例模式：饿汉式、懒汉式、有上限的多例模式

---
+++ src/singleton/Singleton＿Concurrent.java
@@ -0,0 +1,39 @@
+/*
+ * 文件名：Singleton_Common.java
+ * 版权：Copyright 2007-2016 517na Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： Singleton_Common.java
+ * 修改人：xiaofan
+ * 修改时间：2016年4月4日
+ * 修改内容：新增
+ */
+package singleton;
+
+/**
+ * 单例模式多线程注意
+ * 
+ * 增加了synchronized的单例称为懒汉式单例
+ * 
+ * @author xiaofan
+ */
+public class Singleton＿Concurrent {
+    private static Singleton＿Concurrent singleton = null;
+
+    // 限制产生多个对象
+    private Singleton＿Concurrent() {
+
+    }
+
+    // 此模式需注意线程安全,加synchronized
+    // 此方法并不最优，建议使用Singleton单例模式通用代码
+    public static synchronized Singleton＿Concurrent getSinglton() {
+        if (singleton == null) {
+            singleton = new Singleton＿Concurrent();
+        }
+        return singleton;
+    }
+
+    // 类中其他方法，尽量static
+    public static void doSomething() {
+
+    }
+}
